0cd1652081722f60b22c0709d97495999d7ea7cb
harshv5904 <ardostree2001@outlook.com>
2023-10-26 18:29:14 +0530

    feat: Creating Patient List Page

---
+++ src/pages/PatientLists.jsx
@@ -0,0 +1,12 @@
+import Main from "../components/main"
+
+
+function PatientLists() {
+  return (
+    <Main>
+      
+    </Main>
+  )
+}
+
+export default PatientLists
--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import PageNotFound from '../pages/404'
+import PatientLists from '../pages/PatientLists'
 
 function PageRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/*" element={<PageNotFound />} />
+      <Route path="/patients" element={<PatientLists />} />
     </Routes>
   )
 }
