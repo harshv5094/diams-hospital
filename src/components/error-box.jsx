702b1885ae3e41c4c4391ae0460b69779545efea
harshv5904 <harshv5094@gmail.com>
2023-10-28 13:58:57 +0530

    feat: Adding Error Box

---
+++ src/components/error-box.jsx
@@ -0,0 +1,31 @@
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Container
+} from '@chakra-ui/react'
+
+function Error({ message }) {
+  return (
+    <Container mt={`3rem`} size={`container.lg`} centerContent>
+      <Alert
+        status={`error`}
+        variant={`subtle`}
+        flexDir={`column`}
+        alignItems={`center`}
+        justifyContent={`center`}
+        textAlign={`center`}
+        height={`200px`}
+      >
+        <AlertIcon boxSize={`40px`} mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize={`lg`}>
+          Error From The Server!
+        </AlertTitle>
+        <AlertDescription>{message}</AlertDescription>
+      </Alert>
+    </Container>
+  )
+}
+
+export default Error
